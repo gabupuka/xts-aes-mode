69d279d7b095db7bfaa086591509f0644bd9f963
falsewaly <falahmail1@gmail.com>
2018-04-09 17:55:32 +0700

    add method encrypt and decrypt

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
 public class Main {
+    AES myAES = new AES();
+
+    public static void main(String[] args) {
+        byte[]  data;				/* 64-bit AES data */
+        byte[]  key;				/* 128-bit AES key */
+        byte[]  result;				/* 64-bit AES result */
+
+    }
+
+    public byte[] encrypt(byte[] key, byte[] data){
+        byte[]  result;				/* 64-bit AES result */
+        myAES.setKey(key);
+        result = myAES.encrypt(data);
+        return result;
+    }
+
+    public byte[] decrypt(byte[] key, byte[] data){
+        byte[]  result;				/* 64-bit AES result */
+        myAES.setKey(key);
+        result = myAES.decrypt(data);
+        return result;
+    }
 }
